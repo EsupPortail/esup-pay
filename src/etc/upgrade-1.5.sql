6e5cb11879b71cf9072a13289de2f8bf9ae92bb3
Vincent Bonamy <vbonamy@gmail.com>
2022-01-05 10:33:34 +0100

    upgrade-1.5.sql ok

--- src/etc/upgrade-1.5.sql
+++ src/etc/upgrade-1.5.sql
@@ -2,10 +2,7 @@ CREATE TABLE managers_emails (
     pay_evt bigint NOT NULL,
     manager_email character varying(255)
 );
-
 ALTER TABLE managers_emails OWNER TO esuppay;
-
 ALTER TABLE managers_emails ADD CONSTRAINT managers_emails_pay_evt FOREIGN KEY (pay_evt) REFERENCES public.pay_evt(id);
-
 INSERT INTO managers_emails SELECT id, managers_email FROM pay_evt;
-
+ALTER TABLE pay_evt DROP COLUMN managers_email;
