839cd4ef19a440c054aff5689257764e79e9b8e9
Vincent Bonamy <vbonamy@gmail.com>
2022-04-11 18:18:32 +0200

    Contrainte unicité pay_transaction_log.idtrans

--- src/main/java/org/esupportail/pay/domain/PayTransactionLog.java
+++ src/main/java/org/esupportail/pay/domain/PayTransactionLog.java
@@ -18,16 +18,7 @@
 package org.esupportail.pay.domain;
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.persistence.Version;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -38,6 +29,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(uniqueConstraints={@UniqueConstraint(name = "pay_transaction_log_idtrans_unique", columnNames={"idtrans"})})
 public class PayTransactionLog {
 	  
 	@Id
