b9d1ace48ea174056d08f8e2a6e833875cb97eb6
Vincent Bonamy <vbonamy@gmail.com>
2022-06-17 08:19:14 +0200

    Debug - ventilations pouvaient être incorrectes Si 1 evt a un prefixeOtp correspondant au début du prefixeOtp d'un autre evt, les ventilations étaient alors incorrectes.

--- src/main/java/org/esupportail/pay/services/VentilationService.java
+++ src/main/java/org/esupportail/pay/services/VentilationService.java
@@ -103,9 +103,9 @@ public class VentilationService {
 			        @Override
 			        public int compare(String s1, String s2) {
 			            if (s1.length() > s2.length()) {
-			                return 1;
-			            } else if (s1.length() < s2.length()) {
 			                return -1;
+			            } else if (s1.length() < s2.length()) {
+			                return 1;
 			            } else {
 			                return s1.compareTo(s2);
 			            }
