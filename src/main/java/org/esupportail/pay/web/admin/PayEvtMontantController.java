ccb9b39b18f9992195b433e4e92ddbc7a2f1b17c
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2024-05-02 17:14:07 +0200

    chore: remove dead code

--- src/main/java/org/esupportail/pay/web/admin/PayEvtMontantController.java
+++ src/main/java/org/esupportail/pay/web/admin/PayEvtMontantController.java
@@ -141,20 +141,6 @@ public class PayEvtMontantController {
         return "redirect:/admin/evtmnts/" + encodeUrlPathSegment(payEvtMontant.getId().toString(), httpServletRequest);
     }
 
-	@RequestMapping(produces = "text/html")
-    public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
-        if (page != null || size != null) {
-            int sizeNo = size == null ? 10 : size.intValue();
-            final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
-            uiModel.addAttribute("payevtmontants", payEvtMontantDaoService.findPayEvtMontantEntries(firstResult, sizeNo, sortFieldName, sortOrder));
-            float nrOfPages = (float) payEvtMontantDaoService.countPayEvtMontants() / sizeNo;
-            uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
-        } else {
-            uiModel.addAttribute("payevtmontants", payEvtMontantDaoService.findAllPayEvtMontants(sortFieldName, sortOrder));
-        }
-        return "admin/evtmnts/list";
-    }
-
 	@RequestMapping(value = "/{id}", params = "form", produces = "text/html")
     public String updateForm(@PathVariable("id") Long id, Model uiModel) {
         populateEditForm(uiModel, payEvtMontantDaoService.findPayEvtMontant(id));
